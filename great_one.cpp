ed20ba7047df8f3d21b4c6efec1d20213409b9b8
柯敏 <3205244665@qq.com>
2019-07-21 10:42:42 +0800

    求两数中大的一个

---
+++ great_one.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+          int a,b,c;
+	cout <<"This is a program that inputs two Numbers and tells you the larger number.\n";
+        cout <<"The first number:";
+	cin >>a;
+	cout<<"The second number:";
+	cin >>b;
+        c=a>b?a:b;	
+	cout <<"The great one is " <<c <<endl;
+
+
+return 0;
+}
