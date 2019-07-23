5199814c3d050de8f3b98e8535bb6e77b99f9241
柯敏 <3205244665@qq.com>
2019-07-23 11:43:49 +0800

    输出i:1-10,j:10-0.

---
+++ 循环3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+        int i,j;
+        cout <<"输出i:12345678910,j:9876543210";	
+	for(i=0,j=10;i<=10,j>=0;i++,j--)
+	cout <<i << "  " <<j <<endl;
+	
+
+
+return 0;
+}
