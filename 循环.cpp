a91169128a6d0d60e9e1f2e143ec46de9fb38b48
柯敏 <3205244665@qq.com>
2019-07-23 11:01:12 +0800

    给定任意正整数n,输出1-n的平方数.

---
+++ 循环.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+       int i,n;
+       cout <<"nput any given positive integer n, output 1-n squared.\n";       
+       cout <<"n: "; 
+       cin >> n;
+       for(i=1;i<=n;i++)
+       cout<<i*i<<" ";
+       cout <<"\n";
+
+
+
+return 0;
+}
