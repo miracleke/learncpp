8baf0154e809a476bfdd96b29e550621d09a6401
柯敏 <3205244665@qq.com>
2019-07-23 12:02:17 +0800

    求前一百个自然数的和

---
+++ 循环4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+          int i,sum;
+	  sum=0;
+	  cout<<"Sum the first hundred natural Numbers."<<endl; 
+	  for(i=0;i<=100;i++)
+          {
+            sum +=i;
+
+	  }
+          cout <<sum<<endl;
+
+	  
+	 
+return 0;
+}
