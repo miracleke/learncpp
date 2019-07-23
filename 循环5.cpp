fa7fcb1ae7410c306738d6340a0fcee7086854c5
柯敏 <3205244665@qq.com>
2019-07-23 12:22:26 +0800

    一百以内的所有偶数

---
+++ 循环5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+            int i;
+	    cout <<"Find all even Numbers up to 100.\n";
+            for(i=2;i<=100;i+=2)
+            cout <<i<<"  ";
+	    cout <<endl;
+
+
+
+
+
+return 0;
+}
