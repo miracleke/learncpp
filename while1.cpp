171a1183834d0543cebcc38a8620fbe276dbdccd
柯敏 <3205244665@qq.com>
2019-07-24 11:35:36 +0800

    输入任意两个自然数,求它们的最大公约数.

---
+++ while1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a,b,gcd;
+	cin >>a>>b;
+	gcd=a>b?b:a;
+	while (gcd>1&&(a%gcd!=0||b%gcd!=0))
+		gcd--;
+	cout<<gcd<<endl;   
+
+
+
+
+	return 0;
+}
+
