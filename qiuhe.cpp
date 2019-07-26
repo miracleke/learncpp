1d38c5384546240724e644c87210dbf14476aa4a
柯敏 <3205244665@qq.com>
2019-07-26 10:12:57 +0800

    强制类型转换，求和．

---
+++ qiuhe.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int a,b,c;
+	long long s;
+	a = 43245454;
+	b = 5435454;
+	c = 64365464;
+	s = (long long) a + b + c;
+	cout << s << endl;
+
+	return 0;
+}
