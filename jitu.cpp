86617ae510585aba4b1865d8a8483cea75272d75
柯敏 <3205244665@qq.com>
2019-07-27 11:52:26 +0800

    鸡兔同笼.

---
+++ jitu.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int x,y,b,a;
+    a=35;
+    b=94;
+    x=(2*a)-(b/2);
+    y = (b/2)-a;
+    cout << "鸡:" << "  " << x<< "只." << endl;
+    cout << "兔:" << "  " << y << "只." << endl;
+
+    return 0;
+}
