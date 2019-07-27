d926bf4d673da2d3ce8fc7d1de2dc1918bde7fe5
柯敏 <3205244665@qq.com>
2019-07-27 21:55:35 +0800

    输入三条边，输出三角形面积.

--- san.cpp
+++ san.cpp
@@ -3,21 +3,32 @@
 #include<cmath>
 using namespace std;
 int main() {
-	int a,b,c,f,A,B,C;
-	cout <<"输出三角形三边长ａｂｃ（保证能构成三角形）(1<=a,c<=10000)，输出三角形的面积\n";
-	cout <<"a:" <<"  ";
-	cin >>a;
-	cout <<"b:" << "  ";
-	cin >>b;
-	cout <<"c:" <<"  ";
-	cin >>c;
-		if(a<=0||c>10000)
-			cout <<"This number is wrong!\n" <<endl;
-	f=0;
-	f += (a+b+c)/2;
-	A=f-a;     B=f-b;    C=f-c;
+	int a, b, c, f, d, e, g, A, B, C;
+	cout << "输出三角形三边长ａｂｃ（保证能构成三角形）(1<=a,c<=10000)，输出三角形的面积\n";
+	cout << "a:" << "  ";
+	cin >> a;
+	cout << "b:" << "  ";
+	cin >> b;
+	cout << "c:" << "  ";
+	cin >> c;
+	if(b + c <= a|| a + c <= b || a + b <= c)
+        {
+                cout << "不能构成三角形." << endl;
+                return 1;
+        }
 
-	cout <<"面积是：" <<fixed <<setprecision(4) <<sqrt(f*A*B*C)<<endl;
+	if(a <= 0 || c > 10000)
+	{
+		cout << "This number is wrong!\n";
+	        return 2;	
+	}
+	f = 0;
+	f += (a + b + c)/2;
+	A = f - a;    
+	B = f - b;   
+	C = f - c;
+
+	cout <<"面积是：" << fixed << setprecision(4) << sqrt(f * A * B * C)<< endl;
 
 
 	return 0;
