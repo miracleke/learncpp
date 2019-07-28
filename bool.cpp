9a02aa17b15150666faf6d5d9fa6d9f04baf9342
柯敏 <3205244665@qq.com>
2019-07-28 13:23:30 +0800

    布尔数据测试.

---
+++ bool.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    bool found , flag = false;
+    found = true;
+    cout << flag << "  " << found << endl;
+    flag = 5;
+    found = 0;
+    cout << flag << "  " << found << endl;
+
+
+    return 0;
+}
