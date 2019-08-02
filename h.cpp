918b8bbf69ebcabbe1cd0412adc69bb2bd44e62f
柯敏 <3205244665@qq.com>
2019-08-02 14:10:20 +0800

    输出不一样的Ｈello World

---
+++ h.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout <<"*********************"<<endl;
+    cout <<"Hello World!"<<endl;
+    cout <<"*********************"<<endl;
+
+    return 0;
+}
