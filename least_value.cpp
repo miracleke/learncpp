c726171d4ad4c55f18c935a062c20966bb706992
柯敏 <3205244665@qq.com>
2019-08-05 20:06:05 +0800

    :

---
+++ least_value.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i = 1, n, sum = 0;
+    cin >> n;
+    do
+    {
+	 sum += i;
+	 i++;
+    }
+    while (sum < n);
+    cout << i - 1 << endl;
+
+    return 0;
+}
