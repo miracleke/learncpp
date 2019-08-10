b7e803ab79d4a211b892d3972a867aa95bb170d0
柯敏 <3205244665@qq.com>
2019-08-10 12:15:51 +0800

    赋初值探究.

---
+++ 39.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a[5]={1,2};
+    for(int i = 0; i < 5; i++)
+    cout << a[i] << " ";
+    cout << endl;
+    
+    return 0;
+}
+
+
+
