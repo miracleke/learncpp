d85fef7f4850678a3a1e1942825a79416c36ee74
柯敏 <3205244665@qq.com>
2019-08-10 12:00:25 +0800

    比较continue与brake.

---
+++ 32.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i;
+    for(i = 10; i > 0; i--)
+    {
+	 if(i == 5) break;
+         cout << i << endl;
+    }
+
+    return 0;
+}
+
+
+
+
