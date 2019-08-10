f55ebed6b155b039b2855cea1921a3493073ac15
柯敏 <3205244665@qq.com>
2019-08-10 11:52:18 +0800

    将1-9九个数字搭成特殊的三角形.

---
+++ 28.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=1,b=2,c=3,d=4,e=5,f=6,g=7,h=8,i=9;
+    cout << "    " << a << endl;
+    cout << "  " << b << "   " << c << endl;
+    cout << " " << d << "     " << e << endl;;
+    cout << f << "  " << g << " " << h << "  " << i << endl;	
+
+    return 0;
+}
