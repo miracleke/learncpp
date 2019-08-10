301a5019b2d2cd243f859c8fa1c94ba83a3db7ca
柯敏 <3205244665@qq.com>
2019-08-10 23:28:44 +0800

    输入一个数n，输出2+22+222+（n个二）的和.

--- h.cpp
+++ h.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout <<"*********************"<<endl;
-    cout <<"Hello World!"<<endl;
-    cout <<"*********************"<<endl;
+    int a, b, s, d = 0;
+    cin >> a;
+    b = 2;
+    for(s = 1; s <= a; s++)
+    {
+          d += b;
+	  b = b * 10 + 2;
+    }
+    cout << d << endl;
 
     return 0;
 }
