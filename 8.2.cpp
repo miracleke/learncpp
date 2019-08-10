8fcb6eee13b061ad2f2cce17c0f820de5547fd75
柯敏 <3205244665@qq.com>
2019-08-10 10:39:07 +0800

    判断一个数是否为二的次幂.升级版.

---
+++ 8.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n & (n - 1))   cout << "No!" << endl;
+    else cout << "Yes!" << endl;
+
+
+
+
+
+
+return 0;
+}
